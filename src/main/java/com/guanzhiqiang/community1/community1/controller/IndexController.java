9107084681f523b98c4b9ef154016a50e3f719e8
Bycynh1 <bestguanzhiqiang@163.com>
2020-11-12 16:26:48 +0800

    挨add index and bootstrap

---
+++ src/main/java/com/guanzhiqiang/community1/community1/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.guanzhiqiang.community1.community1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author gzq
+ * @date 2020/11/8
+ **/
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
